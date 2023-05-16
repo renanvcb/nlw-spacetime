8e06d1ea93705efa10d5ff57c743784290751f72
Renan Borges <renanvcb@gmail.com>
2023-05-15 22:53:32 -0300

    api setup and starting web app

---
+++ web/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <h1>
+      Hello NLW
+      <Button title="Primeiro" />
+      <Button title="Segundo" />
+      <Button title="Terceiro" />
+    </h1>
+  )
+}
