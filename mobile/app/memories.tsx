fdb1858336a767fc040b28c43ec8d8201881a7df
Renan Borges <renanvcb@gmail.com>
2023-05-22 01:02:30 -0300

    fim aula 4

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from 'react-native'
 
-export default function memories() {
+export default function Memories() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Memories</Text>
--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // Use local ip address
+  baseURL: 'http://192.168.1.6:3333',
 })
